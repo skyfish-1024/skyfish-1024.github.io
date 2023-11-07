815195875adabf13258ad79a3208c7b5da887c9a
skyfish <1982452293@qq.com>
2023-11-07 12:56:14 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		outDir: './docs',
+	},
 	plugins: [vue()],
 	resolve: {
 		alias: {
